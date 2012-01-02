0f0d1389ac7f378f7cf0ffaa8407fd593caef0dd
Tom Scott <tubbo@psychedeli.ca>
2012-01-01 22:51:08 -0500

    added fork synchronization to upgrade script

--- tools/upgrade.sh
+++ tools/upgrade.sh
@@ -1,6 +1,8 @@
 current_path=`pwd`
 printf '\033[0;34m%s\033[0m\n' "Upgrading Oh My Zsh"
-( cd $ZSH && git pull origin master )
+( cd $ZSH && git pull upstream master )
+printf '\033[0;34m%s\033[0m\n' "Synchronizing to fork"
+( cd $ZSH && git push origin master )
 printf '\033[0;32m%s\033[0m\n' '         __                                     __   '
 printf '\033[0;32m%s\033[0m\n' '  ____  / /_     ____ ___  __  __   ____  _____/ /_  '
 printf '\033[0;32m%s\033[0m\n' ' / __ \/ __ \   / __ `__ \/ / / /  /_  / / ___/ __ \ '
