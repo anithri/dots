76bf80391f36930a141b69ce365797b56d91c263
Tom Scott <tom.scott@elocal.com>
2012-01-20 16:12:35 -0500

    moved git plugin code back to plugins/ since it's part of the repo already.
    it's better to use custom/ for machine-specific code rather than fork overrides because custom/ is ignored by git

--- plugins/git/git.plugin.zsh
+++ plugins/git/git.plugin.zsh
@@ -5,6 +5,8 @@ alias gst='git status'
 compdef _git gst=git-status
 alias gl='git pull'
 compdef _git gl=git-pull
+alias glr='git pull --rebase'
+compdef _git gl=git-pull
 alias gup='git fetch && git rebase'
 compdef _git gup=git-fetch
 alias gp='git push'
@@ -26,7 +28,7 @@ alias gcount='git shortlog -sn'
 compdef gcount=git
 alias gcp='git cherry-pick'
 compdef _git gcp=git-cherry-pick
-alias glg='git log --stat --max-count=5'
+alias glg='git l' # see /etc/gitconfig.example for details
 compdef _git glg=git-log
 alias glgg='git log --graph --max-count=5'
 compdef _git glgg=git-log
