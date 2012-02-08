42636a7c3d2cdb3c932276a97b68a7ffe84bbd7c
Aleksey Orekhov <ao2391@columbia.edu>
2012-02-08 15:19:12 -0500

    fixed asterisk display for modified repos in git prior to 1.7.2

--- lib/git.zsh
+++ lib/git.zsh
@@ -8,7 +8,7 @@ function git_prompt_info() {
 # Checks if working tree is dirty
 parse_git_dirty() {
   local SUBMODULE_SYNTAX=''
-  if [[ $POST_1_7_2_GIT -gt 0 ]]; then
+  if [[ PRE_1_7_2_GIT -gt 0 ]]; then
         SUBMODULE_SYNTAX="--ignore-submodules=dirty"
   fi
   if [[ -n $(git status -s ${SUBMODULE_SYNTAX}  2> /dev/null) ]]; then
@@ -69,12 +69,14 @@ git_prompt_status() {
   echo $STATUS
 }
 
+#this is unlikely to change so make it all statically assigned
+PRE_1_7_2_GIT=$(git_compare_version "1.7.2")
 #clean up the namespace slightly by removing the checker function
 unset -f git_compare_version()
 
 #compare the provided version of git to the version installed and on path
 #prints 1 if input version <= installed version
-#prints -1 otherwise
+#prints -1 otherwise 
 function git_compare_version() {
   local INPUT_GIT_VERSION=$1;
   local INSTALLED_GIT_VERSION
