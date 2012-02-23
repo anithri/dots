9f37721249d89c2c834014509130ccf39d70572f
Tom Scott <tom.scott@elocal.com>
2012-02-23 10:14:02 -0500

    added rbtest, a command to test single Ruby files (thanks @robdimarco)

--- plugins/ruby/ruby.plugin.zsh
+++ plugins/ruby/ruby.plugin.zsh
@@ -4,3 +4,36 @@ alias sgem='sudo gem'
 
 # Find ruby file
 alias rfind='find . -name "*.rb" | xargs grep -n'
+
+# Run a ruby test with bare ruby or `bundle exec ruby`
+_rbtest_with_rake() {
+  if [ -f $1 ]; then
+    case `echo $1 | cut -f 2 -d '/'` in
+      unit)
+        task='test:units'
+        ;;
+      functional)
+        task='test:functionals'
+        ;;
+      integration)
+        task='test:integration'
+        ;;
+      *)
+        task='test'
+        ;;
+    esac
+    bundle exec rake $task TEST1
+  else
+    bundle exec rake test
+  fi
+}
+eval "function rbtest () { _rbtest_with_rake \$@}"
+
+_rbtest_without_rake() {
+  if [ -f $1 ]; then
+    bundle exec ruby -Itest $@;
+  else
+    echo "Please specify a file(s) to test"
+  fi;
+}
+eval "function rbtest_exec(){_rbtest_without_rake \$@}"
