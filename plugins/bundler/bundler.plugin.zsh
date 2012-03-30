a6c4d7c08c6672d2c7c819c8d895ffb7111994d4
Tom Scott <tom.scott@elocal.com>
2012-03-30 13:51:40 -0400

    removed most of the meat & potatoes from the bundler plugin as we use RVM mostly and have no need for it

--- plugins/bundler/bundler.plugin.zsh
+++ plugins/bundler/bundler.plugin.zsh
@@ -3,39 +3,39 @@ alias bi="bundle install"
 alias bl="bundle list"
 alias bp="bundle package"
 alias bu="bundle update"
- 
-# The following is based on https://github.com/gma/bundler-exec
-bundled_commands=(annotate cap capify cucumber ey foreman guard heroku middleman nanoc rackup rainbows rails rake rspec ruby shotgun spec spork thin thor unicorn unicorn_rails)
 
-## Functions
+# # The following is based on https://github.com/gma/bundler-exec
+# bundled_commands=(annotate cap capify cucumber ey foreman guard heroku middleman nanoc rackup rainbows rails rake rspec ruby shotgun spec spork thin thor unicorn unicorn_rails)
 
-_bundler-installed() {
-  which bundle > /dev/null 2>&1
-}
+# ## Functions
 
-_within-bundled-project() {
-  local check_dir=$PWD
-  while [ $check_dir != "/" ]; do
-    [ -f "$check_dir/Gemfile" ] && return
-    check_dir="$(dirname $check_dir)"
-  done
-  false
-}
+# _bundler-installed() {
+#   which bundle > /dev/null 2>&1
+# }
 
-_run-with-bundler() {
-  if _bundler-installed && _within-bundled-project; then
-    bundle exec $@
-  else
-    $@
-  fi
-}
+# _within-bundled-project() {
+#   local check_dir=$PWD
+#   while [ $check_dir != "/" ]; do
+#     [ -f "$check_dir/Gemfile" ] && return
+#     check_dir="$(dirname $check_dir)"
+#   done
+#   false
+# }
 
-## Main program
-for cmd in $bundled_commands; do
-  eval "function bundled_$cmd () { _run-with-bundler $cmd \$@}"
-  alias $cmd=bundled_$cmd
+# _run-with-bundler() {
+#   if _bundler-installed && _within-bundled-project; then
+#     bundle exec $@
+#   else
+#     $@
+#   fi
+# }
 
-  if which _$cmd > /dev/null 2>&1; then
-        compdef _$cmd bundled_$cmd=$cmd
-  fi
-done
+# ## Main program
+# for cmd in $bundled_commands; do
+#   eval "function bundled_$cmd () { _run-with-bundler $cmd \$@}"
+#   alias $cmd=bundled_$cmd
+
+#   if which _$cmd > /dev/null 2>&1; then
+#         compdef _$cmd bundled_$cmd=$cmd
+#   fi
+# done
