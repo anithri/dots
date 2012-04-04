1c67fa83eb1f23fb009d176d2662c7888fbbea31
Tom Scott <tom.scott@elocal.com>
2012-04-04 11:07:34 -0400

    example local aliases

--- custom/local.zsh
+++ custom/local.zsh
@@ -1,1 +1,8 @@
 # Functions and aliases you wish to use on this machine only
+
+elocal=$HOME/Code/elocal
+kitchen=$HOME/Code/chef_scripts
+chef_scripts=kitchen
+blog=$HOME/Code/blog
+affiliates=$HOME/Code/affiliates
+diaspora=$HOME/Code/diaspora
